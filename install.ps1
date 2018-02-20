84c089afa217ad5c9f40a3eee20d0f52c70b5a35
orthros <colinnlsn@gmail.com>
2018-02-20 11:01:18 -0600

    Renamed linkPath to startupPath

--- install.ps1
+++ install.ps1
@@ -1,8 +1,8 @@
-$linkPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\"
+$startupPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\"
 
 Get-ChildItem ".\ahk-scripts" -Filter *.ahk | 
     Foreach-Object {
-    $targetPath = [io.path]::combine($linkPath, $_.Name + ".ln")    
+    $targetPath = [io.path]::combine($startupPath, $_.Name + ".ln")
     New-Item -Path $targetPath -ItemType SymbolicLink -Value $_.FullName -Force 
 }
 
